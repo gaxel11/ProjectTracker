bf96598fb9ae8214889377f3560800f858704964
gaxel11 <axel_maicco@hotmail.com>
2023-07-29 13:46:40 -0300

    [#185669615] Hago cambios en la tarea lint y creo la tarea create_tables

--- tasks.py
+++ tasks.py
@@ -1,5 +1,13 @@
-from invoke import task, Collection
+import time
+
+from invoke import Collection, task
+from peewee import OperationalError
 from termcolor import colored
+
+from app.models.record import Record
+from app.models.task import Task
+from app.models.timebar import TimeBar
+from app.models.timebartask import TimeBarTask
 
 
 @task
@@ -9,34 +17,59 @@ def install(c):
         print("Dependencias instaladas.")
     else:
         print("No se encontró el archivo requirements.txt.")
-
+        
 
 @task
 def lint(c):
-    print("--------------Ejecutando la tarea 'lint'--------------")
-    print("Ejecutando 'pylint'...")
+    start_time = time.time()
+    print("--------------Running 'lint' task--------------")
+    print("Running 'pylint'...")
     results = c.run("pylint .", warn=True).stdout.strip().split("\n")
     for result in results:
         print(result)
     
-    print("Ejecutando 'refurb'...")
+    print("Running 'refurb'...")
     results = c.run("refurb .", warn=True).stdout.strip().split("\n")
     for result in results:
         print(result)
     
-    print("Ejecutando 'flake8'...")
+    print("Running 'flake8'...")
     results = c.run("flake8 .", warn=True).stdout.strip().split("\n")
     for result in results:
         print(result)
 
+    end_time = time.time()
+    elapsed_time = end_time - start_time
     if any(result.count(":") > 0 for result in results):
-        print(colored("Errors found. Review previous messages.", "red"))
+        message = (
+            "Errors found. Review previous messages. "
+            f"Elapsed time: {elapsed_time:.2f} seconds"
+        )
+        print(colored(message, "red"))
     else:
-        print(colored("The code is clean", "green"))
-    print("--------------La tarea 'lint' se ha completado---------------")
+        message = (
+            "The code is clean. "
+            f"Elapsed time: {elapsed_time:.2f} seconds"
+        )
+        print(colored(message, "green"))
+
   
+@task
+def create_tables(c):
+    table_classes = [Task, TimeBar, TimeBarTask, Record]
+    if all(table.table_exists() for table in table_classes):
+        print("Tables already exist.")
+    else:
+        try:
+            for table in table_classes:
+                table.create_table()
+            print("Tables created successfully.")
+        except OperationalError as e:
+            print("Error creating tables:", str(e))
+
 
 # Crear la colección principal
 ns = Collection()
 ns.add_task(install, "install")
 ns.add_task(lint, "lint")
+ns.add_task(create_tables, "create_tables")
